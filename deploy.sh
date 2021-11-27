7a8660930b9ca095bb87a03c538c13adb4cca13c
fred <fredckl.dev@gmail.com>
2021-11-27 06:59:56 +0100

    Add deploy file

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+export PUBLIC_URL='https://jsonparser.frederickoller.ch'
+
+# navigate into the build output directory
+cd src
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:fredckl/json-parser-online.git main:gh-pages
+
+cd -
